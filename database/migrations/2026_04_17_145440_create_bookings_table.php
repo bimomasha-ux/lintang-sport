06b9c65190c4749b51f2e65c9233b4794248ea1d
bimo <bimomasha@gmail.com>
2026-09-21 22:45:04 +0700

    terbaru

--- database/migrations/2026_04_17_145440_create_bookings_table.php
+++ database/migrations/2026_04_17_145440_create_bookings_table.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
@@ -13,6 +14,39 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+
+            // Relasi
+            $table->foreignId('patient_id')
+                ->nullable()
+                ->constrained('patients')
+                ->nullOnDelete();
+
+            $table->foreignId('service_id')
+                ->nullable()
+                ->constrained('services')
+                ->nullOnDelete();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            // Data pelanggan
+            $table->string('nama');
+            $table->string('telepon');
+
+            // Jadwal booking
+            $table->date('tanggal');
+            $table->time('jam');
+
+            // Status booking
+            $table->enum('status', [
+                'pending',
+                'diproses',
+                'selesai',
+                'batal'
+            ])->default('pending');
+
             $table->timestamps();
         });
     }
